5c1fe426166dca0b39d066c2841208e43a96772e
Maira Bello <maira.araujo@liferay.com>
2014-08-19 15:11:14 -0300

    renderSoyWeb should render in the correct language
    The renderSoyWeb option only works when locales is null, since it's not passing the language to render to in the soynode.render() call. This change assumes that the caller wants the first locale in locales array to be used in this case.

--- test/test.js
+++ test/test.js
@@ -89,6 +89,27 @@ module.exports = {
       }));
   },
 
+  testCompileTranslatedTemplatesSoyWeb: function(test) {
+    gulp.src(['test/assets/static/soyweb.soy', 'test/assets/foo/soyweb.soy', 'test/assets/valid.soy'])
+      .pipe(soynode({
+        locales: ['pt-BR'],
+        messageFilePathFormat: 'test/assets/translations/translations_pt-BR.xlf',
+        renderSoyWeb: true
+      }))
+      .pipe(gutil.buffer(function(err, files) {
+        test.equal(files.length, 4);
+        assertFilepath(test, files[0], 'soyweb.html');
+        assertFilepath(test, files[1], 'soyweb.html');
+        assertFilepath(test, files[2], 'valid.soy');
+        assertFilepath(test, files[3], 'valid.soy.js');
+        assertFilesize(test, files[0], 531);
+        assertFilesize(test, files[1], 520);
+        assertFilesize(test, files[2], 96);
+        assertFilesize(test, files[3], 346);
+        test.done();
+      }));
+  },
+
   testMessageExtraction: function(test) {
     gulp.src(['test/assets/valid.soy'])
       .pipe(soynode.lang())
