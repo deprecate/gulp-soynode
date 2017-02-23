480a83bef8300dd43c346cb1bfa6910764045314
Fernando Souza <fernando.souza@liferay.com>
2017-02-23 19:37:37 -0300

    Updates for rendering soyweb templates avoiding passing files without it defined

--- test/test.js
+++ test/test.js
@@ -58,20 +58,16 @@ module.exports = {
   },
 
   testCompileTemplatesSoyWeb: function(test) {
-    gulp.src(['test/assets/static/soyweb.soy', 'test/assets/foo/soyweb.soy', 'test/assets/valid.soy'])
+    gulp.src(['test/assets/static/soyweb.soy', 'test/assets/foo/soyweb.soy'])
       .pipe(soynode({
         renderSoyWeb: true
       }))
       .pipe(gutil.buffer(function(err, files) {
-        test.equal(files.length, 4);
+        test.equal(files.length, 2);
         assertFilepath(test, files[0], 'soyweb.html');
         assertFilepath(test, files[1], 'soyweb.html');
-        assertFilepath(test, files[2], 'valid.soy');
-        assertFilepath(test, files[3], 'valid.soy.js');
         assertFilesize(test, files[0], 520);
         assertFilesize(test, files[1], 520);
-        assertFilesize(test, files[2], 96);
-        assertFilesize(test, files[3], 588);
         test.done();
       }));
   },
@@ -155,22 +151,18 @@ module.exports = {
   },
 
   testCompileTranslatedTemplatesSoyWeb: function(test) {
-    gulp.src(['test/assets/static/soyweb.soy', 'test/assets/foo/soyweb.soy', 'test/assets/valid.soy'])
+    gulp.src(['test/assets/static/soyweb.soy', 'test/assets/foo/soyweb.soy'])
       .pipe(soynode({
         locales: ['pt-BR'],
         messageFilePathFormat: 'test/assets/translations/translations_pt-BR.xlf',
         renderSoyWeb: true
       }))
       .pipe(gutil.buffer(function(err, files) {
-        test.equal(files.length, 4);
+        test.equal(files.length, 2);
         assertFilepath(test, files[0], 'soyweb.html');
         assertFilepath(test, files[1], 'soyweb.html');
-        assertFilepath(test, files[2], 'valid.soy');
-        assertFilepath(test, files[3], 'valid.soy.js');
         assertFilesize(test, files[0], 531);
         assertFilesize(test, files[1], 520);
-        assertFilesize(test, files[2], 96);
-        assertFilesize(test, files[3], 588);
         test.done();
       }));
   },
